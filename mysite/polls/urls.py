f9a63e049542fe8db5a39bd57bcc65090cd6ac05
Shipeng Xu <shipeng@objy670.local>
2016-03-30 17:26:19 -0700

    added new files for tutorial part4.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,8 +2,8 @@
 #
 #
 
+'''
 from django.conf.urls import url
-
 from . import views
 
 app_name = 'polls'
@@ -17,5 +17,19 @@ urlpatterns = [
     # ex:  /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
+'''
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
+
 
 
